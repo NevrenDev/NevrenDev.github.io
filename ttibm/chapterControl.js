76e78c13c41cef14418133bedaa6943138ddde5c
NevrenDev <hyp3rpl@gmail.com>
2025-11-05 22:56:23 +0100

    Massive overhaul part 4 - ttibm page, updates page and yet again style tweaking

--- ttibm/chapterControl.js
+++ ttibm/chapterControl.js
@@ -1,54 +1,26 @@
+const ch0 = document.getElementById('ch0');
+const ch1 = document.getElementById('ch1')
+
 function selectChapter(chapter) {
     fetch(chapter+".txt")
         .then((res) => res.text())
         .then((text) => {
-            const field = document.getElementById('read');
             if (chapter != "prolouge") {
                 field.innerHTML = "<h3>Sorry. That chapter isn't available here right now</h3>";
                 return;
             }
-            field.innerHTML = text;
+            if (chapter == "prolouge") {
+                ch0.innerHTML = text;
+                ch0.classList.add('loaded')
+            }
 
         })
         .catch((e) => {
-            document.getElementById('read').innerHTML = "<h3>Sorry. That chapter isn't available here right now</h3>";
+            if (chapter == "prolouge") {
+                ch0.innerHTML = "<h3>Something went wrong with loading</h3>";
+            }
+            else if (chapter == 'chapte1') {
+                ch1.innerHTML = "<h3>WiP - Writing in Progress</h3>"
+            }
         }) 
 }
-
-function changeTheme() {
-    const novel = document.getElementById('read');
-    let theme = novel.className;
-    
-    if (theme == "default") {
-        novel.classList.remove('default');
-        novel.classList.add('light');
-
-        localStorage.theme = "default";
-    } else {
-        novel.classList.add('default');
-        novel.classList.remove('light');
-
-        localStorage.theme = "light";
-    }
-}
-
-function loadTheme() {
-    const theme = localStorage.theme;
-    const novel = document.getElementById('read');
-
-    if (!localStorage.theme) localStorage.theme = "default";
-
-    if (theme == "default") {
-        novel.classList.remove('default');
-        novel.classList.add('light');
-
-        localStorage.theme = "default";
-    } else {
-        novel.classList.add('default');
-        novel.classList.remove('light');
-
-        localStorage.theme = "light";
-    }
-}
-
-document.addEventListener('DOMContentLoaded', loadTheme())
