dd71ab79099d76b40147cce59fa49454b875eb35
NevrenDev <hyp3rpl@gmail.com>
2025-11-06 09:34:23 +0100

    Massive overhaul part 5 - QuickScript page, more style tweaking, legacy files cleanup and theme preservation (saving)

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ function theme() {
     if (html.getAttribute('data-theme') === 'dark') {
         html.removeAttribute('data-theme');
         btn.textContent = 'Theme: Light';
+        localStorage.theme = 'light';
     } else {
         html.setAttribute('data-theme', 'dark');
         btn.textContent = 'Theme: Dark';
+        localStorage.theme = 'dark';
     }
 }
 
 function go(target) {
     window.location = target;
 }
+
+function load_theme() {
+    them = localStorage.theme;
+    const html = document.documentElement;
+
+    if (!localStorage.theme) localStorage.theme = 'light';
+
+    if (them == 'dark') {
+        html.setAttribute('data-theme', 'dark');
+        btn.textContent = 'Theme: Dark';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', load_theme())
